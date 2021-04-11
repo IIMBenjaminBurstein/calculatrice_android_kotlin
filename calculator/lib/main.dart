557f56604d520b593d78618e3e1ac2aabfde8ab3
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2021-04-11 15:26:11 +0100

    flutter

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+import 'dart:math';
 import 'package:calculator/res/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -26,22 +28,24 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  double? input;
-  double? previousInput;
-  String? symbol;
-  static const List<List<String>> grid = <List<String>>[
- <String>["7", "8", "9", "-"],
- <String>["4", "5", "6", "*"],
- <String>["1", "2", "3", "/"],
- <String>["0", ".", "=", "+"],
-];
 
+int previousPoint = 0;
+double? input;
+double? previousInput;
+String? symbol;
+bool? virgule;
 
-
+ static const List<List<String>> grid = <List<String>>[
+  <String>["CE", "C"],
+  <String>["7", "8", "9", "-"],
+  <String>["4", "5", "6", "*"],
+  <String>["1", "2", "3", "/"],
+  <String>["0", ".", "=", "+"],
+]; 
  @override
  Widget build(BuildContext context) {
    return Scaffold(
-      body: DefaultTextStyle(
+   body: DefaultTextStyle(
      style: TextStyle(color: AppColors.white, fontSize: 22.0),
      child: Column(
        children: [
@@ -55,35 +59,35 @@ class _CalculatorState extends State<Calculator> {
            ),
          ),
          Flexible(
-           flex: 8,
+ flex: 8,
 
  // Column = vertical
-              child: Column(
-                // Pour chaque nouvelle ligne, on itère sur chaque cellule
-                children: grid.map((List<String> line) {
-                  return Expanded(
-                    child: Row(
-                        children: line
-                            .map(
-                              (String cell) => Expanded(
-                                child: InputButton(
-                                  label: cell,
-                                  onTap: onItemClicked,
-                                ),
-                              ),
-                            )
-                            .toList(growable: false)),
-                  );
-                }).toList(growable: false),
-              ),
-         )
+ child: Column(
+   // Pour chaque nouvelle ligne, on itère sur chaque cellule
+   children: grid.map((List<String> line) {
+     return Expanded(
+       child: Row(
+           children: line
+               .map(
+                 (String cell) => Expanded(
+                   child: InputButton(
+                     label: cell,
+                     onTap: onItemClicked,
+                   ),
+                 ),
+               )
+               .toList(growable: false)),
+     );
+   }).toList(growable: false),
+ ),
+)
        ],
      ),
    ),
-   );
- }
+ );
+}
 
- void onItemClicked(String value) {
+void onItemClicked(String value) {
  print('On Click $value');
 
  switch (value) {
@@ -105,8 +109,19 @@ class _CalculatorState extends State<Calculator> {
    case '*':
      onNewSymbol(value);
      break;
+
    case '=':
      onEquals();
+     break;
+  case 'CE':
+      onResetValue();
+      break;
+  case 'C':
+      onResetPreviousValue();
+      break;
+  case '.':
+      onPointVirgule();
+      break;
  }
 
  // Force l'interface à se redessiner
@@ -114,21 +129,89 @@ class _CalculatorState extends State<Calculator> {
 }
 
 void onNewDigit(String digit) {
-  // input = '$digit';
-  print(input);
+  double nbr;
+  int x;
+  int dividande;
+
+  if(input == null){
+    input = 0;
+    nbr = input! * 10 + int.parse(digit);
+    input = double.parse(nbr.toString()) ;
+    print("je suis input null");
+  }else if(virgule == true){
+    x = previousPoint + 1;
+    dividande = (pow(10, x)).toInt();
+    print("Avant le calcul $input $dividande $digit");
+    print("calcul:");
+    print((9 / 100) + 9.3);
+    nbr = (int.parse(digit) / dividande) + input!;
+    input = double.parse(nbr.toString());    
+    previousPoint += 1;
+    print(previousPoint);
+    // print("$x $nbr $dividande ");
+    // print(input);
+  }else{
+    nbr = input! * 10 + int.parse(digit);
+    input = double.parse(nbr.toString());
+  }
+  
 }
 
 void onNewSymbol(String digit) {
- symbol = "$digit";
- print(symbol);
+  
+  if(input == 0){
+    previousInput = 0;
+    symbol = digit;
+  }else{
+    symbol = digit;
+    previousInput = input;
+    input = 0;
+    virgule = false;
+    previousPoint = 0;
+  }
 }
 
 void onEquals() {
- // TODO
+
+  if(symbol == '+'){
+    var result = previousInput! + input!;
+    input = result.toDouble();
+  }else if (symbol == '-'){
+    var result = previousInput! - input!;
+    input = result.toDouble();
+  }else if (symbol == '*'){
+    var result = previousInput! * input!;
+    input = result.toDouble();
+  }else if (symbol == '/' ){
+    if(input == 0){
+      input = 0;
+      previousInput= 0;
+      symbol = null;
+    }else{
+      var result = previousInput! / input!;
+    input = result;
+    }
+    
+  }
 }
 
+void onPointVirgule(){
+  virgule = true;
 }
 
+void onResetPreviousValue(){
+  input = 0;
+  virgule = false;
+  previousPoint = 0;
+}
+void onResetValue(){
+  input = 0;
+  symbol = null;
+  previousInput =0; 
+  virgule = false;
+  previousPoint = 0;
+}
+}
 class InputButton extends StatelessWidget {
  final String label;
  final ValueChanged<String>? onTap;
